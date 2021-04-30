133d2519e3e2ed72cc8f3f725e8ee84340392884
dylan <dylanabozarth@gmail.com>
2021-04-29 18:42:46 -0700

    connecting

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 MERN APP, unfinished 
 https://www.youtube.com/watch?v=ngc9gnGgUdA
-9:07
+12:01 
--- back/index.js
+++ back/index.js
@@ -2,3 +2,9 @@ import express from 'express'
 import bodyparser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+const app = express();
+//setting up to send out requests
+app.use(bodyparser.json({limit: "30mb", extended: true}))
+app.use(bodyparser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
