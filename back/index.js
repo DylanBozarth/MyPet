bbd2e565b068b0870906ab21197844b5ef30585f
dylan <dylanabozarth@gmail.com>
2021-04-29 23:36:17 -0700

    found problem

--- back/index.js
+++ back/index.js
@@ -5,19 +5,17 @@ import cors from 'cors'
 
 const app = express();
 //setting up to send out requests
-app.use(bodyparser.json({limit: "30mb", extended: true}))
-app.use(bodyparser.urlencoded({limit: "30mb", extended: true}))
+app.use(bodyparser.json({ limit: "30mb", extended: true }))
+app.use(bodyparser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 //credentials to database
-const CONNECTIONURL = "mongodb+srv://steve:Dontforget1!@cluster0.lyt6y.mongodb.net/cluster0?retryWrites=true&w=majority";
-// port number
-const PORT = process.env.PORT || 5000
-
+const CONNECTIONURL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
 // connecting to database 
-mongoose.connect(CONNECTIONURL, { useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log(`server is at port: ${PORT}`)))
-.catch(() => () => console.log(error.message));
+mongoose.connect(CONNECTIONURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server is at port: ${PORT}`)))
+    .catch(() => () => console.log(error.message));
 
 
 mongoose.set('useFindAndModify', false);
