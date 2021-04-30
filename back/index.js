70ed51c8c09efcb356f5c9f711b051a9d2754dc4
dylan <dylanabozarth@gmail.com>
2021-04-29 23:15:48 -0700

    connection issues

--- back/index.js
+++ back/index.js
@@ -8,3 +8,16 @@ const app = express();
 app.use(bodyparser.json({limit: "30mb", extended: true}))
 app.use(bodyparser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
+
+//credentials to database
+const CONNECTIONURL = "mongodb+srv://steve:Dontforget1!@cluster0.lyt6y.mongodb.net/cluster0?retryWrites=true&w=majority";
+// port number
+const PORT = process.env.PORT || 5000
+
+// connecting to database 
+mongoose.connect(CONNECTIONURL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`server is at port: ${PORT}`)))
+.catch(() => () => console.log(error.message));
+
+
+mongoose.set('useFindAndModify', false);
