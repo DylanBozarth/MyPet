34847b63a5f2d8455052edafeeb5f9b3044e485d
dylan <dylanabozarth@gmail.com>
2021-05-06 09:49:45 -0700

    dao controllers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 MERN APP, unfinished 
 https://www.youtube.com/watch?v=mrHNSanmqQ4
-34:04
-fcc is life
+51:11
+fcc is lifes
--- back/api/restuarants.route.js
+++ back/api/restuarants.route.js
@@ -1,7 +1,13 @@
 import express from 'express' 
 import RestaurantsController from './restaurants.controller.js'
+import ReviewsController from './reviews.controller.js'
 const router = express.Router();
 
 router.route('/').get(RestaurantsController.apiGetRestaurants)
 // this is where the view is set. use ?zipcode=000 to filter in browser
+router 
+.route('/review')
+.post(ReviewsController.apiPostReview)
+.put(ReviewsController.apiUpdateReview)
+.delete(ReviewsController.apiDeleteReview)
 export default router
