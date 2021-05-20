29995ea3eca9d2e68c3309aab88ab5cd4cb8f5fa
dylan <dylanabozarth@gmail.com>
2021-05-19 17:40:09 -0700

    incoming webhook not found

--- front/src/components/submit.js
+++ front/src/components/submit.js
@@ -17,16 +17,27 @@ export const Submit = () => {
     const imagestuff = e.target.value;
     setPetImage(imagestuff);
   };
-  const submitPet = () => {
-    console.log(petName, petBreed, petImage);
+  const submitPet = (e) => {
+    e.preventDefault();
+   //('https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/my_pets-dbdsd/service/pets/incoming_webhook/addnewpet')
+    let _data = {
+      title: "foo",
+      body: petName
+    }
+    
+    fetch('https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/my_pets-dbdsd/service/pets/incoming_webhook/addnewpet'
+      , {
+      method: "POST",
+      body: JSON.stringify(_data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json));
   };
   return (
     <div>
       Submit your own
-      <form
-        className=""
-        action="https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/my_pets-dbdsd/service/pets/incoming_webhook/petswebhook"
-        method="post"
+      <div
       >
         <label>Your pet's name</label>
         <input
@@ -46,8 +57,8 @@ export const Submit = () => {
           value={petImage}
           onChange={changePetImage}
         ></input>
-        <button onClick={() => submitPet()}>Submit</button>
-      </form>
+        <button onClick={(e) => submitPet(e)}>Submit</button>
+      </div>
     </div>
   );
 };
