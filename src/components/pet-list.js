10c33abf91eb1c6e4b758570e653b39314c5ac88
dylan <dylanabozarth@gmail.com>
2021-06-23 07:34:16 -0700

    card 1

--- src/components/pet-list.js
+++ src/components/pet-list.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { gsap } from "gsap";
 const RestaurantsList = (props) => {
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
     getPetInfo();
-   setTimeout((MoveItMoveIt()), 500)
+   
+   
+    
   }, []);
-const MoveItMoveIt = () => {
-   gsap.from('.gallery-item', { delay: .2,  duration: 3, y: 110, ease: "elastic(1, 0.5)", stagger: '0.4'})
-}
+
 
     
   async function getPetInfo() {
@@ -21,8 +20,15 @@ const MoveItMoveIt = () => {
     const json = await response.json();
     console.log(json);
     setPets(json.pets);
+    MoveItMoveIt()
   }
-
+  const MoveItMoveIt = () => { 
+    const box = document.getElementsByClassName('.gallery-item')
+    
+      gsap.from('.gallery-item', { delay: .4,  duration: 3, y: 110, ease: "elastic(1, 0.5)", stagger: '0.4'})
+    
+       
+  }
   const deletePet = (_id) => {
     axios
       .delete(
@@ -50,8 +56,8 @@ const MoveItMoveIt = () => {
         <div className="row">
           {pets.map((pets) => {
             return (
-              <div className="col-sm-4 gallery-item  text-center">
-                <div className="" key={pets.user} id={pets._id}>
+              <div className="col-sm-4 gallery-item  text-center" key={pets.user}>
+                <div className=""  id={pets._id}>
                   <div className="">
                     <h3 className="gallery-text">{pets.pet}</h3>
                     <h4 className="gallery-text">{pets.breed}</h4>
@@ -89,24 +95,15 @@ const MoveItMoveIt = () => {
         <div className="row ">
         {pets.map((pets) => {
             return (
-              <div className="col-sm-4 gallery-item  text-center">
-                <div className="" key={pets.user}>
-                  <div className="">
-                    <h3 className="gallery-text">{pets.pet}</h3>
-                    <h4 className="gallery-text">{pets.breed}</h4>
-                    <p className="gallery-text">
-                      submitted by: {pets.user}
-                    </p>{" "}
-                    <img
-                      src={pets.image}
-                      className=" img-fluid gallery-image"
-                      alt={pets.pet}
-                    ></img>
-                    <p className="gallery-text">Fun fact about {pets.pet}:</p>
-                    <p className="gallery-text">{pets.desc}</p>
-                  </div>
-                </div>
-              </div>
+              <div class="flip">
+              <div class="front" style={{backgroundImage: `url${pets.image}`}}>
+       <h1 class="text-shadow">MOUNTAIN</h1>
+    </div>
+    <div class="back">
+       <h2>Angular</h2>
+       <p>Good tools make application development quicker and easier to maintain than if you did everything by hand..</p>
+    </div>
+</div>
             );
           })}
           
